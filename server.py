d87b723c05e7cc1ee7e69c3155ba0838f3b94eb2
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-05-05 21:43:20 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import random
+app = Flask (__name__)
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def random_color():
+    return render_template("index.html",random_number = [1,2,3,4])  # Return the string 'Hello World!' as a response
+
+
+    
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)
